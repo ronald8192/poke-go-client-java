b58f3e6bc929cfcc33f08642b5088bd9b184ba78
ronald8192 <u23805@gmail.com>
2016-08-12 09:07:33 +0800

    v1

---
+++ src/main/java/com/ronald8192/pokemon/App.java
@@ -0,0 +1,181 @@
+package com.ronald8192.pokemon;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App extends JFrame {
+
+	private static final long serialVersionUID = 8742922210164223685L;
+
+	private static Logger log = LoggerFactory.getLogger(App.class);
+
+	private static PokeFetcher poke = null;
+	private static String googleAuthUrl = "";
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				poke = new PokeFetcher();
+				new App().createView().initListeners().setVisible(true);
+			}
+		});
+		Runtime.getRuntime().addShutdownHook(new Thread("shutdown") {
+			@Override
+			public void run() {
+				poke = null;
+				System.gc();
+			}
+		});
+
+	}
+
+	private static JPanel mainPanel;
+	private static GridBagConstraints c;
+	private static String uiTitle = "PokeDetails";
+	private static JLabel lblGoogleAuth;
+	private static JButton btnOpenAuth, btnDownload;
+	private static JTextField txtAuthToken;
+
+	private void prepareView() {
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new GridBagLayout());
+		getContentPane().add(mainPanel);
+		c = new GridBagConstraints();
+
+		c.gridx = 0;
+		c.gridy = 0;
+	}
+
+	private App createView() {
+		prepareView();
+
+		btnOpenAuth = new JButton("Auth with Google");
+		mainPanel.add(btnOpenAuth);
+
+		c.gridy++;
+		c.gridy++;
+		lblGoogleAuth = new JLabel("Paste your token below and click download.");
+		mainPanel.add(lblGoogleAuth, c);
+
+		c.gridx = 0;
+		c.gridy++;
+		txtAuthToken = new JTextField(28);
+		mainPanel.add(txtAuthToken, c);
+
+		c.gridx = 0;
+		c.gridy++;
+		btnDownload = new JButton("Download");
+		mainPanel.add(btnDownload, c);
+
+		afterCreateView();
+		return this;
+	}
+
+	private void afterCreateView() {
+		setTitle(uiTitle);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+	}
+
+	private App initListeners() {
+		btnOpenAuth.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+//				log.debug(googleAuthUrl);
+				
+				btnOpenAuth.setEnabled(false);
+				btnOpenAuth.setText("Getting Google OAuth link...");
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						googleAuthUrl = poke.getLoginToken();
+						openWebPage(googleAuthUrl);
+						btnOpenAuth.setEnabled(true);
+						btnOpenAuth.setText("Auth with Google");
+					}
+				}).start();
+				
+				
+			}
+		});
+
+		btnDownload.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnDownload.setEnabled(false);
+				btnDownload.setText("Downloading...");
+
+				new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						String token = txtAuthToken.getText();
+						DownloadError err = null;
+						if (poke.isLoggedIn()) {
+							err = poke.download();
+						} else {
+							if (token.trim().equals("")) {
+								JOptionPane.showMessageDialog(mainPanel, "Please authenticate with Google first.");
+							} else {
+								try {
+									poke.login(token);
+									err = poke.download();
+								} catch (Exception er) {
+									log.error(er.getMessage());
+									err = DownloadError.AUTH_ERROR;
+								}
+								
+							}
+						}
+						if(err != null){
+							JOptionPane.showMessageDialog(mainPanel, err.say());
+						}
+						btnDownload.setText("Download");
+						new Thread(new Runnable() {
+							@Override
+							public void run() {
+								try {
+									Thread.sleep(3000L);
+								} catch (InterruptedException e) {
+								}finally{
+									btnDownload.setEnabled(true);
+								}
+							}
+						}).start();
+						
+					}
+				}).start();
+
+			}
+		});
+
+		return this;
+	}
+
+	private static void openWebPage(String url) {
+		try {
+			java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+		} catch (java.io.IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+}
---
+++ src/main/java/com/ronald8192/pokemon/PokeFetcher.java
@@ -0,0 +1,203 @@
+package com.ronald8192.pokemon;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.text.DecimalFormat;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.api.player.PlayerProfile;
+import com.pokegoapi.api.pokemon.Pokemon;
+import com.pokegoapi.auth.GoogleUserCredentialProvider;
+import com.pokegoapi.exceptions.LoginFailedException;
+import com.pokegoapi.exceptions.NoSuchItemException;
+import com.pokegoapi.exceptions.RemoteServerException;
+
+import okhttp3.OkHttpClient;
+
+public class PokeFetcher {
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+
+	private DecimalFormat df = new DecimalFormat("####.##");
+	private DecimalFormat dfPercent = new DecimalFormat("#.####");
+
+	private OkHttpClient httpClient = new OkHttpClient();
+	private GoogleUserCredentialProvider provider = null;
+
+	private boolean loggedIn = false;
+	private PokemonGo pokemonGo;
+
+	public String getLoginToken() {
+		try {
+			provider = new GoogleUserCredentialProvider(httpClient);
+		} catch (LoginFailedException e) {
+			e.printStackTrace();
+		} catch (RemoteServerException e) {
+			e.printStackTrace();
+		}
+		// in this url, you will get a code for the google account that is
+		// logged
+		log.info("Please go to " + GoogleUserCredentialProvider.LOGIN_URL);
+		return GoogleUserCredentialProvider.LOGIN_URL;
+	}
+
+	public void login(String access) throws Exception {
+		// we should be able to login with this token
+		try {
+			provider.login(access);
+			pokemonGo = new PokemonGo(provider, httpClient);
+		} catch (Exception e) {
+			loggedIn = false;
+			throw e;
+		}
+		
+		loggedIn = true;
+		
+	}
+
+	public DownloadError download()  {
+		// After this you can access the api from the PokemonGo instance :
+		try {
+			// PokeBank pokeBank = go.getInventories().getPokebank();
+
+			// to get all his inventories (Pokemon, backpack, egg,
+			// incubator)
+			
+			JSONObject pokeDetails = new JSONObject();
+			
+			PlayerProfile playerProfile= pokemonGo.getPlayerProfile();
+			JSONObject player = new JSONObject();
+			player.put("name", playerProfile.getPlayerData().getUsername());
+			pokeDetails.put("player", player);
+			
+			List<Pokemon> pokemons = pokemonGo.getInventories().getPokebank().getPokemons();
+			JSONArray myPokemons = new JSONArray();
+			for (Pokemon pokemon : pokemons) {
+				JSONObject p = new JSONObject();
+				p.put("poke_id", pokemon.getMeta().getNumber());
+				p.put("nickname", (pokemon.getNickname()));
+				p.put("lv", pokemon.getLevel());
+				p.put("cp", pokemon.getCp());
+				p.put("move_1", humanize(pokemon.getMove1().toString()));
+				p.put("move_2", humanize(pokemon.getMove2().toString()));
+				p.put("iv", Double.parseDouble(dfPercent.format(pokemon.getIvRatio())));
+				p.put("iv_a", pokemon.getIndividualAttack());
+				p.put("iv_d", pokemon.getIndividualDefense());
+				p.put("iv_s", pokemon.getIndividualStamina());
+				p.put("height", Double.parseDouble(df.format(pokemon.getHeightM())));
+				p.put("weight", Double.parseDouble(df.format(pokemon.getWeightKg())));
+				p.put("fav", pokemon.isFavorite());
+				p.put("created_at", pokemon.getCreationTimeMs());
+				try {
+					p.put("max_cp",pokemon.getMaxCp());
+				} catch (JSONException | NoSuchItemException e1) {
+					p.put("max_cp","");
+					e1.printStackTrace();
+				}
+				try {
+					p.put("max_cp_fp", pokemon.getMaxCpForPlayer());
+				} catch (JSONException | NoSuchItemException e) {
+					p.put("max_cp_fp", "");
+					e.printStackTrace();
+				}
+				p.put("max_cp_fepufp",pokemon.getMaxCpFullEvolveAndPowerupForPlayer());
+				myPokemons.put(p);
+			}
+			pokeDetails.put("pokemons", myPokemons);
+
+			BufferedWriter writer = null;
+			boolean hasError = false;
+			try {
+				
+				writer = new BufferedWriter(new PrintWriter(getJarDir() + "/myPokemon.json"));
+				writer.write(pokeDetails.toString());
+			} catch (IOException e) {
+				hasError = true;
+			} finally {
+				try {
+					if (writer != null)
+						writer.close();
+				} catch (IOException e) {
+				}
+			}
+
+
+			if (hasError) {
+				log.error("Save file error.");
+				log.info(myPokemons.toString());
+				return DownloadError.SAVE_TO_DISK_ERROR;
+			} else {
+				log.info("Your Pokemon data has saved to 'myPokemon.json', please copy the content to https://ronald8192.github.io/pokemon-go-detail-visualizer/");
+			}
+
+			// System.out.println();
+			// System.out.println("Currencies:");
+			// Map currencies = go.getPlayerProfile().getCurrencies();
+			// for (Object o:currencies.keySet()) {
+			// PlayerProfile.Currency c = (PlayerProfile.Currency)o;
+			// System.out.println(c.name() + " : " + currencies.get(o));
+			// }
+
+		} catch (RemoteServerException | NullPointerException e) {
+			log.error(e.getMessage());
+			return DownloadError.NETOWRK_ERROR;
+		}catch (LoginFailedException e){
+			log.error(e.getMessage());
+			return DownloadError.AUTH_ERROR;
+		}
+		return DownloadError.NO_ERROR;
+	}
+
+	public boolean isLoggedIn(){
+		return this.loggedIn;
+	}
+	
+	private static String humanize(String s) {
+		if (s.length() == 0)
+			return s;
+
+		String[] words = s.split("_");
+		s = "";
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].equals("FAST")) {
+				s += "";
+			} else {
+				s += (words[i].charAt(0) + "").toUpperCase() + words[i].substring(1).toLowerCase();
+			}
+			if (i + 1 != words.length)
+				s += " ";
+		}
+		return s.trim();
+	}
+	
+	/**
+	 * Get the jar file(program itself) directory
+	 * @return jar directory
+	 */
+	private static String getJarDir() {
+		URL url = PokeFetcher.class.getProtectionDomain().getCodeSource().getLocation(); //Gets the path
+	  	String jarPath = null;
+		try {
+			jarPath = URLDecoder.decode(url.getFile(), "UTF-8"); //Should fix it to be read correctly by the system
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+			
+		//Path of the jar	
+	    return new File(jarPath).getParentFile().getPath(); 
+	    
+	}
+
+}
